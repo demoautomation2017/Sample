3093cf465ec0e471476e3e6805bf9d7097687791
bingibittu <bingibittu@LAPTOP-QVJ1HV8G.hsd1.ca.comcast.net>
2017-07-11 16:30:36 -0700

    Added the Login and SucessfulLogin Test cases and reusable methods

--- src/main/java/com/testing/Cliniops/Cliniops_ReusableMethods.java
+++ src/main/java/com/testing/Cliniops/Cliniops_ReusableMethods.java
@@ -1,3 +1,286 @@
 package com.testing.Cliniops;
 
-public class Cliniops_ReusableMethodsTest { }
+public class Cliniops_ReusableMethodsTest { 
+
+	static BufferedWriter bw = null;
+	static BufferedWriter bw1 = null;
+	static String htmlname;
+	static String objType;
+	static String objName;
+	static String TestData;
+	static String rootPath;
+	static int report;
+
+
+	static Date cur_dt = null;
+	static String filenamer;
+	static String TestReport;
+	int rowcnt;
+	static String exeStatus = "True";
+	static int iflag = 0;
+	static int j = 1;
+
+	static String fireFoxBrowser;
+	static String chromeBrowser;
+
+	static String result;
+
+	static int intRowCount = 0;
+	static String dataTablePath;
+	static int i;
+	static String browserName;
+	/* 
+	 * Name of the Method: enterText
+	 * Brief description : Enter text into text box field
+	 * Arguments: obj --> Webelement Object, textval --> Text to be entered, objName --> name of hte object
+	 * Created by: Automation team
+	 * Creation date : July 17 2017
+	 * last modified:  2017
+	 * */
+	public static void enterText(WebElement obj, String textVal, String objName) throws IOException{
+		if(obj.isDisplayed()){
+			obj.sendKeys(textVal);
+			Update_Report("Pass", "enterText", textVal + " is entered in " + objName);
+			
+		}else{
+			Update_Report("Fail", "enterText", objName + " field is not displayed, please check your application.");
+			
+		}
+
+	}
+	/* 
+	 * Name of the Method: dropDown
+	 * Brief description : Selecting the DropDown
+	 * Arguments: obj --> Webelement Object, textval --> Text to be entered 
+	 * Created by: Automation team
+	 * Creation date : July 17 2017
+	 * last modified:  July 17 2017
+	 * */
+	public static void dropDown(WebElement dd, int index){
+		
+		Select select = new Select(dd);
+		if(dd.isDisplayed()){
+			select.selectByIndex(index);
+			System.out.println("Pass" +dd+ "is selected");
+		}
+			else{
+				System.out.println("Fail" +dd + "is not displayed");
+			}
+		
+	}
+
+	/* 
+	 * Name of the Method: clickObj
+	 * Brief description : click object
+	 * Arguments: obj --> Webelement Object,  objName --> name of the object
+	 * Created by: Automation team
+	 * Creation date : July 17 2017
+	 * last modified: July 17 2017
+	 * 
+	 * */	
+	public static void clickObj(WebElement obj, String objName){
+		if(obj.isDisplayed()){
+			obj.click();
+			System.out.println("Pass: "+ objName + " is clicked.");
+		}else{
+			System.out.println("Fail: " + objName+ " is not displayed, please check your application");
+		}
+	}
+
+	/* 
+	 * Name of the Method: validateMsg
+	 * Brief description : validate message displayed on the web page
+	 * Arguments: obj --> Webelement Object,  expectedText --> expected text to be displayed, objName --> name of hte object
+	 * Created by: Automation team
+	 * Creation date : July 17 2017
+	 * last modified: July 17 2017
+	 * 
+	 * */	
+
+	public static void validateMsg(WebElement obj, String expectedText, String objName){
+		if(obj.isDisplayed()){
+			String actualText = obj.getText().trim();
+			if(expectedText.equals(actualText)){
+				System.out.println("Pass: Actual message is matching with expected message " + actualText);
+			}else{
+				System.out.println("Fail: Actual message '" + actualText + "' is not matching with expected message '"+ expectedText+"'  ,Please check your application");
+			}
+		}else{
+			System.out.println("Fail: " + objName+ " is not displayed, please check your application");
+		}
+	}
+	/* Method Name : readSheet
+	 * Brief Desc: Read XL content
+	 * Arguments: testDataPath --> Test Data path, sheetName --> Name of the Sheet
+	 * Created By: Automation team
+	 * Creation Date: July 17 2017
+	 * Last Modified: July 17 2017
+	 * */
+
+	public static String[][] readXlSheet(String dtPath, String sheetName) throws IOException{
+		/*Step 1: Get the XL Path*/
+		File xlFile = new File(dtPath);
+
+		/*Step2: Access the Xl File*/
+		FileInputStream xlDoc = new FileInputStream(xlFile);
+
+		/*Step3: Access the work book */
+		HSSFWorkbook wb = new HSSFWorkbook(xlDoc);
+
+		/*Step4: Access the Sheet */
+		HSSFSheet sheet = wb.getSheet(sheetName);
+
+		int iRowCount = sheet.getLastRowNum()+1;
+		int iColCount = sheet.getRow(0).getLastCellNum();
+
+		System.out.println("Rows = " + iRowCount+ ",   col = " + iColCount);
+
+
+		String[][] xlData = new String[iRowCount][iColCount];
+
+		for(int i = 0; i<iRowCount; i++){
+			for(int j = 0; j<iColCount; j++){
+				xlData[i][j] = sheet.getRow(i).getCell(j).getStringCellValue();
+			}
+
+		}
+
+		return xlData;
+	}
+	
+	/* Method Name : writeXlSheet
+	 * Brief Desc: Write in to  XL sheet
+	 * Arguments: testDataPath --> Test Data path, sheetName --> Name of the Sheet, textVal --> Text to be entered, iRow --> Row number, iCol --> column number
+	 * Created By: Automation team
+	 * Creation Date: July 17th 2017
+	 * Last Modified: July 17th 2017
+	 * */
+	public static void writeXlSheet(String dtPath, String sheetName, String textVal, int iRow, int iCol ) throws IOException{
+		/*Step 1: Get the XL Path*/
+		File xlFile = new File(dtPath);
+
+		/*Step2: Access the Xl File*/
+		FileInputStream xlDoc = new FileInputStream(xlFile);
+
+		/*Step3: Access the work book */
+		HSSFWorkbook wb = new HSSFWorkbook(xlDoc);
+
+		/*Step4: Access the Sheet */
+		HSSFSheet sheet = wb.getSheet(sheetName);
+
+		/*Step 5: Access Row*/
+		HSSFRow row = sheet.getRow(iRow);
+
+		/*Access cell*/
+		HSSFCell cell = row.getCell(iCol);
+
+//		cell.setCellValue(textVal);
+
+		if(textVal.equals("Pass")){
+			HSSFCellStyle titleStyle = wb.createCellStyle();
+			titleStyle.setFillForegroundColor(new HSSFColor.GREEN().getIndex());
+			titleStyle.setFillPattern(CellStyle.SOLID_FOREGROUND);
+			cell.setCellStyle(titleStyle);
+		}else{
+			HSSFCellStyle titleStyle = wb.createCellStyle();
+			titleStyle.setFillForegroundColor(new HSSFColor.RED().getIndex());
+			titleStyle.setFillPattern(CellStyle.SOLID_FOREGROUND);
+			cell.setCellStyle(titleStyle);
+		}
+		
+
+
+		FileOutputStream fout = new FileOutputStream(dtPath);
+		wb.write(fout);
+		fout.flush();
+		fout.close();
+	}
+	
+	
+	
+	public static void startReport(String scriptName, String ReportsPath) throws IOException{
+
+		String strResultPath = null;
+
+
+		String testScriptName =scriptName;
+
+
+		cur_dt = new Date(); 
+		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss");
+		String strTimeStamp = dateFormat.format(cur_dt);
+
+		if (ReportsPath == "") { 
+
+			ReportsPath = "C:\\";
+		}
+
+		if (ReportsPath.endsWith("\\")) { 
+			ReportsPath = ReportsPath + "\\";
+		}
+
+		strResultPath = ReportsPath + "Log" + "/" +testScriptName +"/"; 
+		File f = new File(strResultPath);
+		f.mkdirs();
+		htmlname = strResultPath  + testScriptName + "_" + strTimeStamp 
+				+ ".html";
+
+
+
+		bw = new BufferedWriter(new FileWriter(htmlname));
+
+		bw.write("<HTML><BODY><TABLE BORDER=0 CELLPADDING=3 CELLSPACING=1 WIDTH=100%>");
+		bw.write("<TABLE BORDER=0 BGCOLOR=BLACK CELLPADDING=3 CELLSPACING=1 WIDTH=100%>");
+		bw.write("<TR><TD BGCOLOR=#66699 WIDTH=27%><FONT FACE=VERDANA COLOR=WHITE SIZE=2><B>Browser Name</B></FONT></TD><TD COLSPAN=6 BGCOLOR=#66699><FONT FACE=VERDANA COLOR=WHITE SIZE=2><B>"
+				+ "FireFox " + "</B></FONT></TD></TR>");
+		bw.write("<HTML><BODY><TABLE BORDER=1 CELLPADDING=3 CELLSPACING=1 WIDTH=100%>");
+		bw.write("<TR COLS=7><TD BGCOLOR=#BDBDBD WIDTH=3%><FONT FACE=VERDANA COLOR=BLACK SIZE=2><B>List Number</B></FONT></TD>"
+				+ "<TD BGCOLOR=#BDBDBD WIDTH=10%><FONT FACE=VERDANA COLOR=BLACK SIZE=2><B>Step Name</B></FONT></TD>"
+				+ "<TD BGCOLOR=#BDBDBD WIDTH=10%><FONT FACE=VERDANA COLOR=BLACK SIZE=2><B>Execution Time</B></FONT></TD> "
+				+ "<TD BGCOLOR=#BDBDBD WIDTH=10%><FONT FACE=VERDANA COLOR=BLACK SIZE=2><B>Status</B></FONT></TD>"
+				+ "<TD BGCOLOR=#BDBDBD WIDTH=47%><FONT FACE=VERDANA COLOR=BLACK SIZE=2><B>Detail Report</B></FONT></TD></TR>");
+
+
+	}
+
+	public static void Update_Report(String Res_type,String Action, String result) throws IOException {
+		String str_time;
+		Date exec_time = new Date();
+		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss");
+		str_time = dateFormat.format(exec_time);
+		if (Res_type.startsWith("Pass")) {
+			bw.write("<TR COLS=7><TD BGCOLOR=#EEEEEE WIDTH=3%><FONT FACE=VERDANA SIZE=2>"
+					+ (j++)
+					+ "</FONT></TD><TD BGCOLOR=#EEEEEE WIDTH=10%><FONT FACE=VERDANA SIZE=2>"
+					+Action
+					+ "</FONT></TD><TD BGCOLOR=#EEEEEE WIDTH=10%><FONT FACE=VERDANA SIZE=2>"
+					+ str_time
+					+ "</FONT></TD><TD BGCOLOR=#EEEEEE WIDTH=10%><FONT FACE=VERDANA SIZE=2 COLOR = GREEN>"
+					+ "Passed"
+					+ "</FONT></TD><TD BGCOLOR=#EEEEEE WIDTH=30%><FONT FACE=VERDANA SIZE=2 COLOR = GREEN>"
+					+ result + "</FONT></TD></TR>");
+
+		} else if (Res_type.startsWith("Fail")) {
+			exeStatus = "Failed";
+			report = 1;
+			bw.write("<TR COLS=7><TD BGCOLOR=#EEEEEE WIDTH=3%><FONT FACE=VERDANA SIZE=2>"
+					+ (j++)
+					+ "</FONT></TD><TD BGCOLOR=#EEEEEE WIDTH=10%><FONT FACE=VERDANA SIZE=2>"
+					+Action
+					+ "</FONT></TD><TD BGCOLOR=#EEEEEE WIDTH=10%><FONT FACE=VERDANA SIZE=2>"
+					+ str_time
+					+ "</FONT></TD><TD BGCOLOR=#EEEEEE WIDTH=10%><FONT FACE=VERDANA SIZE=2 COLOR = RED>"
+					+ "<a href= "
+					+ htmlname
+					+ "  style=\"color: #FF0000\"> Failed </a>"
+
+				+ "</FONT></TD><TD BGCOLOR=#EEEEEE WIDTH=30%><FONT FACE=VERDANA SIZE=2 COLOR = RED>"
+				+ result + "</FONT></TD></TR>");
+
+		} 
+		
+		
+		
+		
+	}
+}
--- src/test/java/com/testing/Cliniops/Cliniops_AutomationScriptsTest.java
+++ src/test/java/com/testing/Cliniops/Cliniops_AutomationScriptsTest.java
@@ -1,4 +1,55 @@
 package com.testing.Cliniops;
 
 public class Cliniops_AutomationScriptsTest extends Cliniops_ReusableMethodsTest{
+
+WebDriver dr;
+	
+	public void loginErrorMessage1() throws IOException{
+		
+		dr.get("https://bridgetherapeutics.cliniops.com");
+		
+		WebElement username= dr.findElement(By.id("username"));
+		enterText(username, "Abhishek", "Username field");
+		
+		WebElement pwd= dr.findElement(By.id("password"));
+		enterText(pwd, "welcome", "Password field");
+		
+		WebElement authBtn= dr.findElement(By.id("Authenticate"));
+		clickObj(authBtn, "Authenticate Button");
+		
+		WebElement errorMsg=dr.findElement(By.className("error"));
+		String error= errorMsg.getText();
+		String expectedText="Please enter the user name";
+		
+		validateMsg(errorMsg, expectedText, error);
+	
+	}
+	public void loginErrorMessage1() throws IOException{
+		
+		dr.get("https://bridgetherapeutics.cliniops.com");
+		
+		WebElement username= dr.findElement(By.id("username"));
+		enterText(username, "Abhishek", "Username field");
+		
+		WebElement pwd= dr.findElement(By.id("password"));
+		enterText(pwd, "welcome", "Password field");
+		
+		WebElement authBtn= dr.findElement(By.id("Authenticate"));
+		clickObj(authBtn, "Authenticate Button");
+		
+		
+		WebElement dd1=dr.findElement(By.id("investigator_study"));
+		dropDown(dd1, 0);
+
+		WebElement dd2=dr.findElement(By.name("lang_type"));
+		Select select = new Select(dd2);
+		//dropDown(dd2, 0);
+		select.selectByValue("1");
+		
+		WebElement clickLogin= dr.findElement(By.className("login-btnew disable"));
+		clickObj(clickLogin, "Login");
+
+
+
+
 }
