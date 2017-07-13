48b32d63668c9013486a512be1a0cb3090797826
skiranmai <skiranmai78@gmail.com>
2017-07-13 06:34:28 -0700

    Added changed files

--- src/test/java/com/testing/Cliniops/Cliniops_DriverScriptTest.java
+++ src/test/java/com/testing/Cliniops/Cliniops_DriverScriptTest.java
@@ -2,5 +2,7 @@ package com.testing.Cliniops;
 
 public class Cliniops_DriverScriptTest extends Cliniops_AutomationScriptsTest {
 
-	public static void main(String[] args){ }
+	public static void main(String[] args){ 
+		
+	}
 }
