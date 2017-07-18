ccbfaeaecec395bd8b1321f2510edc4937ee575a
skiranmai <skiranmai78@gmail.com>
2017-07-17 23:30:40 -0700

    Added Cliniops_DriverScriptTest.java file also updated pom.xml and ReusableMethodsTest.java file

--- src/test/java/com/testing/Cliniops/Cliniops_ReusableMethodsTest.java
+++ src/test/java/com/testing/Cliniops/Cliniops_ReusableMethodsTest.java
@@ -1,11 +1,21 @@
 package com.testing.Cliniops;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Cliniops_ReusableMethodsTest { 
+	
+	static WebDriver driver ;
+	
 
 	/* 
 	 * Name of the Method: enterText
@@ -86,5 +96,56 @@ public class Cliniops_ReusableMethodsTest {
 		}else{
 			System.out.println("Fail: " + objName+ " is not displayed, please check your application");
 		}
-	}		
+	}
+	
+//	/*
+//	 * Name of method : toBeClickable
+//	 * Brief description : wait for the given time until the webelement is located
+//	 * Arguments:
+//	 * Created By :
+//	 * Creation Date
+//	 * Last Modified:
+//	 */
+//	
+//	public static void toBeClickable(int timeInMilliSec, WebElement element){
+//		
+//		WebDriverWait wait = new WebDriverWait(driver, timeInMilliSec);
+//		WebElement elementToClick = wait.until(ExpectedConditions.elementToBeClickable(element));
+//		elementToClick.click();
+//		System.out.println("clicked on the given element....");
+//		
+//	}
+	
+	public static String[][] readSheet(String dataTable, String sheetName) throws IOException{
+
+
+		/*Step 1: Get the XL Path*/
+		File xlFile = new File(dataTable);
+
+		/*Step2: Access the Xl File*/
+		FileInputStream xlDoc = new FileInputStream(xlFile);
+
+		/*Step3: Access the work book */
+		HSSFWorkbook wb = new HSSFWorkbook(xlDoc);
+
+
+		/*Step4: Access the Sheet */
+		HSSFSheet sheet = wb.getSheet(sheetName);
+
+		int iRowCount =  sheet.getLastRowNum()+1;
+		int iColCount = sheet.getRow(0).getLastCellNum();//Row count starts from '0' in excel
+
+		System.out.println("Total row = " + iRowCount + " total col = " + iColCount);
+
+		String[][] xlData = new String[iRowCount][iColCount];
+
+		for(int i =0; i<iRowCount;i++){
+			for(int j = 0; j <iColCount;j++){
+				xlData[i][j] = sheet.getRow(i).getCell(j).getStringCellValue().trim();
+			}
+
+		}
+		
+		return xlData;
+	}
 	}
